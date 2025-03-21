5495c19e3374578aecefb51362bcf431570ad78e
lucas-duval <61875366+lucas-duval@users.noreply.github.com>
2025-03-21 16:11:06 +0100

    Update test_dag.py

--- dags/harvest/test_dag.py
+++ dags/harvest/test_dag.py
@@ -1,1 +1,17 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.providers.snowflake.operators.snowflake import SQLExecuteQueryOperator
+import os
 
+# Configuration du DAG
+default_args = {
+    'owner': 'airflow',
+    'start_date': days_ago(1),
+    'retries': 1
+}
+dag = DAG(
+    'eco2mix_snowflake_dag',
+    default_args=default_args,
+    description='DAG pour extraire et charger des données Eco2mix dans Snowflake',
+    schedule_interval='@daily'
+)

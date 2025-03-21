e3c637887e5ddb4e390fb079a89ac2d48ac33a9b
lucas-duval <61875366+lucas-duval@users.noreply.github.com>
2025-03-21 15:56:39 +0100

    Update harvest_eco2mix.py

--- dags/harvest/harvest_eco2mix.py
+++ dags/harvest/harvest_eco2mix.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from airflow.utils.dates import days_ago
-from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from airflow.providers.snowflake.operators.snowflake import SQLExecuteQueryOperator
 from eco2mix_operator import Eco2MixOperator
 import os
 

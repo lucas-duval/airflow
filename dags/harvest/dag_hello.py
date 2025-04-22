46efd52ab7fb9466cdfcadc90c32c837dcd67393
lucas-duval <61875366+lucas-duval@users.noreply.github.com>
2025-04-22 14:53:31 +0200

    Update dag_hello.py

--- dags/harvest/dag_hello.py
+++ dags/harvest/dag_hello.py
@@ -1,21 +1,5 @@
-# hello_world_dag.py
-
 from airflow import DAG
-from airflow.operators.python import PythonOperator
 from datetime import datetime
 
-def say_hello():
-    print("Hello, world!")
-
-with DAG(
-    dag_id='hello_world',
-    start_date=datetime(2024, 1, 1),
-    schedule_interval=None,  # Manual trigger
-    catchup=False,
-    tags=["example"],
-) as dag:
-
-    hello_task = PythonOperator(
-        task_id='say_hello_task',
-        python_callable=say_hello,
-    )
+with DAG('my_dag_id', start_date=datetime(2023, 1, 1), schedule_interval='@daily') as dag:
+    pass

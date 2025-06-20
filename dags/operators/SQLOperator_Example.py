8bbcff22caed263a0efa7acbf74d9234c709c054
duval <lucas.duval@etu.univ-amu.fr>
2025-06-20 03:44:25 +0200

    .

--- dags/operators/SQLOperator_Example.py
+++ dags/operators/SQLOperator_Example.py
@@ -4,9 +4,10 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 from datetime import datetime
+import os
 
 # === FONCTION définie en haut ===
-def create_table_from_csv(csv_file, table_name, database_name, schema_name, conn_id):
+def create_table_from_csv(csv_file, table_name, database_name, schema_name, stage_name, auto_compress, overwrite, conn_id):
     df = pd.read_csv(csv_file)
 
     def pandas_to_snowflake_type(dtype):
@@ -45,6 +46,24 @@ def create_table_from_csv(csv_file, table_name, database_name, schema_name, conn
     finally:
         cursor.close()
         conn.close()
+        
+    if not os.path.exists(csv_file):
+        raise FileNotFoundError(f"Fichier introuvable : {csv_file}")
+
+    hook = SnowflakeHook(snowflake_conn_id=conn_id)
+
+    hook.run(f"USE DATABASE {database_name}")
+    hook.run(f"USE SCHEMA {schema_name}")
+
+    put_cmd = (
+        f"PUT file://{csv_file} @{database_name}.{schema_name}.{stage_name} "
+        f"{'AUTO_COMPRESS=TRUE' if auto_compress else ''} "
+        f"{'OVERWRITE=TRUE' if overwrite else ''};"
+    )
+
+    print(f"Exécution de la commande PUT : {put_cmd}")
+    hook.run(put_cmd)
+    print("Upload vers le stage terminé.")
 
 # === DAG défini ensuite ===
 with DAG(
@@ -55,13 +74,16 @@ with DAG(
 ) as dag:
 
     create_table = PythonOperator(
-        task_id="create_table_from_csv",
+        task_id="create_ingest_table_from_csv",
         python_callable=create_table_from_csv,
         op_kwargs={
             "csv_file": "/opt/airflow/data/eco2mix_data.csv",
             "table_name": "RTE_RAW_DATA",
             "database_name": "BRONZE",
             "schema_name": "RTE",
+            "stage_name": "RTE_STAGE",
+            "auto_compress": "TRUE",
+            "overwrite": "TRUE",
             "conn_id": "SnowflakeConnection"
         }
     )
